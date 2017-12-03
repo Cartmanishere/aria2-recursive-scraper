801bd3d28a8d3791c37fc62fa713355fe3d17a48
Cartmanishere <apbetahouse45@gmail.com>
2017-12-03 20:09:55 +0530

    fixed bug

--- download.py
+++ download.py
@@ -1,7 +1,12 @@
 import os
 
+def download():
+	os.system("aria2c --file-allocation=none -c --auto-file-renaming=false -i links.txt")
+
 folders = [ x[0] for x in os.walk(os.getcwd()) ]
+folders.sort()
+download()
 
 for i in folders:
 	os.chdir(i.encode("utf-8").decode())
-	os.system("aria2c --file-allocation=none -c --auto-file-renaming=false -i links.txt")
+	download()
--- download.sh
+++
@@ -1,9 +0,0 @@
-cwd=$PWD
-find . -type d -print0 |
-  while IFS= read -rd '' dir;
-do
-	cd "$dir"
-	echo "Downloading from ${dir}"
-	aria2c -c --auto-file-renaming=false -i links.txt
-	cd "$cwd"
-done
